1fb51c76d2238f9c5b323f275a3867144b47929a
seejn <seejnmaharjan@gmail.com>
2025-01-30 19:47:59 +0545

    added page: PlaceOrder

---
+++ web/src/pages/PlaceOrderPage/PlaceOrderPage.jsx
@@ -0,0 +1,125 @@
+// import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+import { useCart } from 'src/hooks/useCart'
+import { useAuth } from 'src/hooks/useAuth'
+import {v4 as uuidv4} from "uuid"
+
+const ItemRow = ({item}) => {
+  return (
+    <>
+      <tr>
+        <td className="py-3 px-4 border-b">{item.menu.name}</td>
+        <td className="py-3 px-4 border-b">{item.quantity}</td>
+        <td className="py-3 px-4 border-b">Rs. {item.total_price}</td>
+      </tr>
+    </>
+  )
+}
+
+const PlaceOrderPage = () => {
+  const { items, getTotal } = useCart()
+  const {getUser} = useAuth()
+  const {id: userId} = getUser()
+  console.log(items)
+
+  const createOrderPayload = () => {
+    const updatedItems = items.map(item => {
+      return{
+        menuId: item.menu.id,
+        image: item.image,
+        quantity: item.quantity,
+        price: item.price,
+        totalPrice: item.total_price,
+        restaurantId: item.restaurantId,
+    }
+    })
+    return {
+      userId: userId,
+      totalAmount: getTotal(),
+      paymentMethod: "CASH_ON_DELIVERY",
+      orderedItems: updatedItems
+    }
+  }
+
+  const itemsUI = items.length > 0 ? items.map(item => <ItemRow key={uuidv4()}  item={item} />) : <p className='text-red-400 text-center text-xl font-bold'>No Items in Cart</p>
+
+  const handleConfirmOrder = (e) => {
+    e.preventDefault()
+    console.log(createOrderPayload())
+  }
+
+  return (
+    <>
+      <Metadata title="PlaceOrder" description="PlaceOrder page" />
+
+      <div className="max-w-full p-6">
+        <header className="mb-8">
+          <h1 className="text-3xl font-semibold text-[#04AA6D]">Payment Details</h1>
+        </header>
+
+        <div className="flex gap-10 bg-white p-6 rounded-lg shadow-md space-y-8">
+          <div>
+            <h2 className="text-2xl font-semibold text-[#32a4e7]">Order Summary</h2>
+            <table className="w-full mt-4 border-collapse">
+              <thead>
+                <tr>
+                  <th className="py-3 px-4 border-b text-left text-lg">Item</th>
+                  <th className="py-3 px-4 border-b text-left text-lg">Quantity</th>
+                  <th className="py-3 px-4 border-b text-left text-lg">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {itemsUI}
+              </tbody>
+            </table>
+          </div>
+          <div>
+            {/* <div className="mt-6 flex justify-between">
+              <h3 className="text-xl font-medium">Subtotal</h3>
+              <span className="text-lg">$22.00</span>
+            </div>
+            <div className="mt-2 flex justify-between">
+              <h3 className="text-xl font-medium">Delivery Fee</h3>
+              <span className="text-lg">$5.00</span>
+            </div> */}
+            <div className="mt-2 flex justify-between">
+              <h3 className="text-xl font-medium">Grand Total</h3>
+              <span className="text-lg font-semibold">Rs. {getTotal()}</span>
+            </div>
+
+            <div className="mt-6">
+              <h2 className="text-2xl font-semibold text-[#32a4e7]">
+                Payment Method
+              </h2>
+              <form className="mt-4">
+                <div className="flex items-center space-x-4">
+                  <input
+                    type="radio"
+                    id="cash-on-delivery"
+                    name="payment-method"
+                    value="cash-on-delivery"
+                    className="h-5 w-5 text-[#04AA6D]"
+                    checked
+                  />
+                  <label htmlFor="cash-on-delivery" className="text-lg">
+                    Cash on Delivery
+                  </label>
+                </div>
+
+                <button
+                  type="submit"
+                  className="mt-6 bg-[#04AA6D] text-white px-6 py-3 rounded-md w-full"
+                  onClick={handleConfirmOrder}
+                >
+                  Confirm Order
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default PlaceOrderPage
---
+++ web/src/pages/PlaceOrderPage/PlaceOrderPage.stories.jsx
@@ -0,0 +1,9 @@
+import PlaceOrderPage from './PlaceOrderPage'
+
+const meta = {
+  component: PlaceOrderPage,
+}
+
+export default meta
+
+export const Primary = {}
